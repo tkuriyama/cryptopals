e241ed84855fd87f16d1db0762a9e9ec6fa3c9cc
THK <taro.kuriyama@gmail.com>
2017-04-01 19:01:01 -0400

    Refactoring to use utils

--- set1/p5.py
+++ set1/p5.py
@@ -1,15 +1,4 @@
-from __future__ import division
-
-def expand_key(key, length):
-    """Expand key to string of given length by repetition."""
-    return key * int(length / len(key)) + key[:length % len(key)]
-
-def encode_xor(text, key):
-    """Encrypt plaintext w/ key using repeating XOR; return as hex."""
-    full_key = expand_key(key, len(text))
-    xor_arr = [c1 ^ c2 for c1, c2 in zip(bytearray(text), bytearray(full_key))]
-    xor_str = ''.join([chr(c).encode('hex') for c in xor_arr])
-    return xor_str
+import utils
 
 def main():
     """Main."""
@@ -22,8 +11,8 @@ def main():
     code += '272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b2028316'
     code += '5286326302e27282f'
 
-    print encode_xor(source, key)
-    assert encode_xor(source, key) == code
+    xor_arr = utils.xor(bytearray(source), bytearray(key))
+    assert ''.join([chr(c).encode('hex') for c in xor_arr]) == code
     print 'p5 ok'
 
 if __name__ == '__main__':
