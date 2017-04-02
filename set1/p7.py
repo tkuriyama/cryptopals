7d55251654e9b0bafbb09c1d27b350e76776943c
THK <taro.kuriyama@gmail.com>
2017-04-01 21:58:58 -0400

    adding p7

---
+++ set1/p7.py
@@ -0,0 +1,17 @@
+from Crypto.Cipher import AES
+import utils
+
+def main():
+    """Main."""
+
+    with open('p7_problem.txt', 'r') as f:
+        data = f.read()
+    code = data.decode('base64')
+
+    key = 'YELLOW SUBMARINE'
+    decryptor = AES.new(key, AES.MODE_ECB)
+    print decryptor.decrypt(code)
+    print 'p7 ok'
+
+if __name__ == '__main__':
+    main()
