86b7e2c5a6193233847c2a10076b45cca547d73d
THK <taro.kuriyama@gmail.com>
2017-04-09 11:20:45 -0400

    adding set2/p1

---
+++ set2/p1.py
@@ -0,0 +1,11 @@
+import utils
+
+def main():
+    """Main."""
+    key = 'YELLOW SUBMARINE'
+    padded = 'YELLOW SUBMARINE\x04\x04\x04\x04'
+    assert utils.pad_pkcs7(key, 20) == padded
+    print 'p1 ok'
+
+if __name__ == '__main__':
+    main()
