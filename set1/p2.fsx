d307b6340ee460aa94ab7cae34fe198d0f2c15f0
Taro Kuriyama <taro.kuriyama@gmail.com>
2017-05-20 22:34:53 -0400

    refactoring with Utils.fsx

--- set1/p2.fsx
+++ set1/p2.fsx
@@ -1,32 +1,11 @@
-let hexToByte = function
-    | '0' -> 0uy  | '1' -> 1uy
-    | '2' -> 2uy  | '3' -> 3uy
-    | '4' -> 4uy  | '5' -> 5uy
-    | '6' -> 6uy  | '7' -> 7uy
-    | '8' -> 8uy  | '9' -> 9uy
-    | 'a' -> 10uy | 'b' -> 11uy
-    | 'c' -> 12uy | 'd' -> 13uy
-    | 'e' -> 14uy | 'f' -> 15uy
-    | _ -> failwith "Invalid hex char"
-
-let chunk n xs =
-    xs 
-    |> Seq.mapi (fun i x -> i/n, x)
-    |> Seq.groupBy fst
-    |> Seq.map (fun (_, g) -> Seq.map snd g)
-
-let decodeHex s =
-    s
-    |> chunk 2
-    |> Seq.map (fun pair -> (Seq.head pair, Seq.last pair))
-    |> Seq.map (fun (x, y) -> (hexToByte x <<< 4) ||| hexToByte y)
-    |> List.ofSeq
+#load "Utils.fs"
+open Utils
 
 let xor (b1: byte list) (b2: byte list) : byte seq =
     Seq.zip b1 b2
     |> Seq.map (fun (x, y) -> x ^^^ y)
 
-let bytesToStr (b: byte seq) : String =
+let bytesToStr (b: byte seq) : string =
     Seq.map (sprintf "%02x") b
     |> String.concat ""
 
@@ -35,4 +14,4 @@ let s2 = "686974207468652062756c6c277320657965"
 let target = "746865206b696420646f6e277420706c6179"
 
 let test =
-    (xor (decodeHex s1) (decodeHex s2) |> bytesToStr) = target
+    (xor (Utils.decodeHex s1) (Utils.decodeHex s2) |> bytesToStr) = target
---
+++ set1/utils.fs
@@ -0,0 +1,33 @@
+module Utils
+
+let hexToByte = function
+    | '0' -> 0uy  | '1' -> 1uy
+    | '2' -> 2uy  | '3' -> 3uy
+    | '4' -> 4uy  | '5' -> 5uy
+    | '6' -> 6uy  | '7' -> 7uy
+    | '8' -> 8uy  | '9' -> 9uy
+    | 'a' -> 10uy | 'b' -> 11uy
+    | 'c' -> 12uy | 'd' -> 13uy
+    | 'e' -> 14uy | 'f' -> 15uy
+    | _ -> failwith "Invalid hex char"
+
+let chunk n xs =
+    xs 
+    |> Seq.mapi (fun i x -> i/n, x)
+    |> Seq.groupBy fst
+    |> Seq.map (fun (_, g) -> Seq.map snd g)
+
+let decodeHex s =
+    s
+    |> chunk 2
+    |> Seq.map (fun pair -> (Seq.head pair, Seq.last pair))
+    |> Seq.map (fun (x, y) -> (hexToByte x <<< 4) ||| hexToByte y)
+    |> List.ofSeq
+
+let xor (b1: byte list) (b2: byte list) : byte seq =
+    Seq.zip b1 b2
+    |> Seq.map (fun (x, y) -> x ^^^ y)
+
+let bytesToStr (b: byte seq) : String =
+    Seq.map (sprintf "%02x") b
+    |> String.concat ""
