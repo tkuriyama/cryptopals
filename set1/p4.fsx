38a5ba722679112fbd8e390d9965247561ace0af
THK <taro.kuriyama@gmail.com>
2017-05-26 21:52:47 -0400

    refactoring

--- set1/p4.fsx
+++ set1/p4.fsx
@@ -1,15 +1,8 @@
 #load "Utils.fs"
 
 open Utils
-open System.IO
-
-let readLines (filePath: string) =
-    seq { use sr = new StreamReader (filePath)
-          while not sr.EndOfStream do
-          yield sr.ReadLine () }
 
 let solve =
-    readLines "p4_problem.txt"
-    |> Seq.map Utils.decodeHex
-    |> Seq.map Utils.decryptSingleXor
+    Utils.readLines "p4_problem.txt"
+    |> Seq.map (Utils.decodeHex >> Utils.decryptSingleXor)
     |> Seq.maxBy (snd)
