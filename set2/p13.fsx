68df8e0559e91cc627a28f53e241982725871e56
THK <taro.kuriyama@gmail.com>
2017-09-21 20:36:32 -0400

    sanitize email

--- set2/p13.fsx
+++ set2/p13.fsx
@@ -3,13 +3,16 @@
 open Utils
 
 let profileParse (s: string) =
-    let pairs = s.Split([|'&'; '='|])
-    let e = Array.length pairs - 1
+    let elems = s.Split([|'&'; '='|])
+    let e = Array.length elems - 1
     [| for i in [0..e]
-           do if i % 2 = 0 then yield (pairs.[i], pairs.[i+1]) |]
+           do if i % 2 = 0 then yield (elems.[i], elems.[i+1]) |]
     |> Map.ofArray
 
 let encodeEmail (email: string) =
-    let s = String.concat "" ["email="; email; "&uid=10&role=user"]
+    let sanitized =
+        let elems = email.Split([|'@'; '&'; '='|])
+        if Array.length elems > 2 then elems.[0] else email
+    let s = String.concat "" ["email="; sanitized; "&uid=10&role=user"]
     profileParse s
                        
