04f7811f3a18538d2442430f084ee821c06eafe9
THK <taro.kuriyama@gmail.com>
2017-09-21 20:52:27 -0400

    encrypt and decrypt per problem

--- set2/p13.fsx
+++ set2/p13.fsx
@@ -15,4 +15,20 @@ let encodeEmail (email: string) =
         if Array.length elems > 2 then elems.[0] else email
     let s = String.concat "" ["email="; sanitized; "&uid=10&role=user"]
     profileParse s
-                       
+
+let profile_for (email: string) =
+    let profile = encodeEmail email
+    String.concat "" ["email="; profile.["email"];
+                      "&uid="; profile.["uid"];
+                      "&role="; profile.["role"]]
+
+let key = Utils.randKey 16 |> Utils.bytesToStr
+let iv = Utils.randKey 16
+let profile = profile_for "test@test.com"
+let encrypted = Utils.AESEncryptECB key iv (profile |> Utils.strToBytes)
+let decrypted = Utils.AESDecryptECB key encrypted |> Utils.bytesToStr
+
+let adminProfile =
+    genAdmin key
+    |> Utils.AESDecryptECB key
+    |> Utils.bytesToStr
