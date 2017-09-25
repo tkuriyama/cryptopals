22a77a0ebe6b327a3f0480d3890bda60912b8a6e
THK <taro.kuriyama@gmail.com>
2017-09-25 08:16:37 -0400

    padding function, fix index

--- set3/p17.fsx
+++ set3/p17.fsx
@@ -28,23 +28,25 @@ let paddingOracle (code: byte []) : bool =
     
 let testDecrypt = testEncrypt |> paddingOracle
 
+let genPadding n = [| for n in [1..n] do yield byte n |]
+
 let decryptLastByte code ind1 ind2 =
     1
     
-let decryptByte code ind1 ind2 offset =
+let decryptByte code ind1 ind2 offset found =
     1
 
 let rec decryptBlock ind1 ind2 offset (found: byte []) (code: byte []): byte [] =
     match offset with
         | -1 -> found |> Array.rev
-        | 15 -> let b = decryptLastByte code ind1 ind2
+        | 15 -> let bs = decryptLastByte code ind1 ind2
                 let n = Array.length b
-                decryptBlock code ind1 ind2 (offset-n) (Array.append found b)
-        | _  -> let b = decryptByte code ind1 ind2 offset
+                decryptBlock code ind1 ind2 (offset-n) (Array.append found bs)
+        | _  -> let b = decryptByte code ind1 ind2 offset found
                 decryptBlock code ind1 ind2 (offset-1) (Array.append found b)
 
 let CBCPaddingDecrypt (code: byte []) =
     let numBlocks = Array.chunkBySize 16 code |> Array.length
-    [| for i in [0..(numBlocks-2)] do yield
+    [| for i in [0..(numBlocks-1)] do yield
            Array.append iv code |> decryptBlock (i*16) ((i+1)*16) 15 [] |]
     |> Array.concat
