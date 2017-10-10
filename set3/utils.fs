e3ad8feafdbaf736001a2183dc619964c986d828
THK <taro.kuriyama@gmail.com>
2017-10-09 21:58:12 -0400

    repeated key xor decryption

--- set3/utils.fs
+++ set3/utils.fs
@@ -200,3 +200,43 @@ let applyCTR (key: string) (nonce: byte []) (code: byte []) : byte [] =
     let numBlocks = Array.chunkBySize 16 code |> Array.length
     genStream key nonce numBlocks 0 [||]
     |> xorArr code
+
+(* Decrypt single-char XOR *)
+
+let freqMap =
+    Map.ofList [('a', 0.0651738); ('b', 0.0124248); ('c', 0.0217339);
+                ('d', 0.0349835); ('e', 0.1041442); ('f', 0.0197881);
+                ('g', 0.0158610); ('h', 0.0492888); ('i', 0.0558094);
+                ('j', 0.0009033); ('k', 0.0050529); ('l', 0.0331490);
+                ('m', 0.0202124); ('n', 0.0564513); ('o', 0.0596302);
+                ('p', 0.0137645); ('q', 0.0008606); ('r', 0.0497563);
+                ('s', 0.0515760); ('t', 0.0729357); ('u', 0.0225134);
+                ('v', 0.0082903); ('w', 0.0171272); ('x', 0.0013692);
+                ('y', 0.0145984); ('z', 0.0007836); (' ', 0.1918182)]
+
+let singleXorGuesses (code: byte seq) : (string * string) seq  =
+    seq{ for b in 00uy .. 255uy do
+         let guess = xor code (repeat b) 
+         yield ([|b|] |> bytesToStr, guess |> bytesToStr) }
+
+let chiSquared (observation: float) (expected: float) =
+    (pown (observation - expected) 2) / expected
+
+let scoreGuesses (guesses: (string * string) seq) : ((string * string) * float) seq =
+    let lookupKey k v n =
+        match (Map.tryFind (Char.ToLower k) freqMap) with
+        | Some x -> chiSquared (float v) (n * x)
+        | None   -> 99.99
+    let score n hist =
+        Map.fold (fun s k v -> s + lookupKey k v n) 0.0 hist
+        // Map.fold (fun s k v -> s + (lookupKey k v)) 0.0 hist
+    guesses
+    |> Seq.map (fun (key, guess) ->
+                    ((key, guess),
+                     histogram guess |> score (String.length guess |> float)))
+
+let decryptSingleXor (code: byte seq) =
+    code
+    |> singleXorGuesses
+    |> scoreGuesses
+    |> Seq.minBy snd
