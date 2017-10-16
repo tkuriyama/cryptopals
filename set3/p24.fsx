8bd5871cef482a7541189558c2c4e5a333a07590
Taro Kuriyama <taro.kuriyama@gmail.com>
2017-10-15 22:04:22 -0400

    initial commit

--- set3/p24.fsx
+++ set3/p24.fsx
@@ -1,11 +1,30 @@
 #load "utils.fs"
 
 open Utils
+open System
 
-let code = "Yellow submarine, testing testing" 
+let testCode = "Yellow submarine, testing testing" 
 
 let applyCTR (seed: uint32) (code: byte []) : byte [] =
     Utils.xorArr (MTSequence seed |> Seq.map byte) code
 
-let encrypted = Utils.strToBytes code |> applyCTR 0u
-let decrypted = applyCTR 0u encrypted |> Utils.bytesToStr
+let encryptTest = Utils.strToBytes testCode |> applyCTR 0u
+let decryptTest = applyCTR 0u encryptTest |> Utils.bytesToStr
+
+let rnd = Random()
+let seed = let max = pown 2 16 in rnd.Next(max) |> uint32
+let input = 
+    "AAAAAAAAAAAAAA"
+    |> Utils.strToBytes
+    |> Array.append (Utils.randKey (5 + rnd.Next(5)))
+let code = applyCTR seed input
+
+let tryFind 
+
+let findSeed code input : int =
+    let rec f code i =
+        match i with
+        | 65536 -> 65536
+        | _     -> let found = tryFind code i
+                   if found = true then i else f code (i+1)
+    f code 1
