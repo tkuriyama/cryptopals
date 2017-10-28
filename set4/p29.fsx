f11480e55900d676ddc82996d32bd7f9f32ea62a
Taro Kuriyama <taro.kuriyama@gmail.com>
2017-10-28 05:58:24 -0400

    adding hacked version of preprocess

--- set4/p29.fsx
+++ set4/p29.fsx
@@ -4,6 +4,8 @@
 open Utils
 open Sha1
 
+(* Prpare Inputs *)
+
 let key = Utils.randKey 16
 let target = ";admin=True" |> Utils.strToBytes
 let targetBytes =
@@ -11,9 +13,19 @@ let targetBytes =
     Array.append padding target
 
 let originalMsg = "comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon" |> Utils.strToBytes
+
+let preprocessHack (data: byte []) : byte [] =
+    let dataPadded = Utils.repeatArr 0uy 16 |> Array.append data
+    let pl = Sha1.padLen dataPadded
+    if pl = 0 then data
+    else let zeros = Utils.repeatArr 0uy (pl - 3)
+         Array.concat [| data; [|128uy|]; zeros; (Sha1.msgLen dataPadded) |]
+
 let targetMsg =
-    Array.append (Sha1.preprocess originalMsg) targetBytes
-                                       
+    Array.append (preprocessHack originalMsg) targetBytes
+
+(* SHA1-MAC Forgery *)
+
 let MAC k m = Array.append k m |> Sha1.sha1
 
 let genState (hash: string) =
