0d94e1c3a595aa059ecfa9d5cfedc01e61128c1b
THK <taro.kuriyama@gmail.com>
2017-11-20 21:32:02 -0500

    BigInteger refactor

--- set5/p39.fsx
+++ set5/p39.fsx
@@ -4,17 +4,27 @@ open Utils
 open System
 open System.Numerics
 
-let primes = Utils.readLines "primes.csv" |> Seq.map int
+let primes = Utils.readLines "primes_10K.txt" |> Seq.map int |> Seq.map BigInteger
 
 let r = new Random()
-let p = Seq.take (r.Next(1000)) primes |> Seq.last;;
-let q = Seq.take (r.Next(1000)) primes |> Seq.last;;
+let p = Seq.take (5000 + (r.Next(5000))) primes |> Seq.last 
+let q = Seq.take (5000 + (r.Next(5000))) primes |> Seq.last 
 let n = p * q
 
-let et = (p - 1) * (q - 1)
-let e = 3
-let d = Utils.modInv e et
+let et = (p - (BigInteger 1)) * (q - (BigInteger 1))
+let e = BigInteger 3
+let d = Utils.modInvBig e et
 
-let c = BigInteger.ModPow (BigInteger 42, BigInteger e, BigInteger n)
-let m = BigInteger.ModPow (c, BigInteger (d.Value), BigInteger n)
+(* Test Int *)
+
+let c = BigInteger.ModPow (BigInteger 42, e, n)
+let m = BigInteger.ModPow (c, d.Value, n)
+
+(* Test String *)
+
+let msg = "Test" |> Utils.strToBytes |> BigInteger
+let code = BigInteger.ModPow (msg, BigInteger e, BigInteger n)
+let plain =
+    let b = BigInteger.ModPow (code, BigInteger (d.Value), BigInteger n)
+    b.ToByteArray() |> Utils.bytesToStr
 
