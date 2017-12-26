cfc44cafcf0fe8ded6ec3ecec62abc5cf4d86cbf
tkuriyama <taro.kuriyama@gmail.com>
2017-12-25 21:11:40 -0500

    signature verification

--- set6/p42.fsx
+++ set6/p42.fsx
@@ -13,8 +13,14 @@ let digest = Sha1.sha1 text
 let block =
     let r = 128 - 3 - ((String.length digest) / 2)
     String.concat "" ["0001"; String.replicate r "ff"; "00"; digest]
-let signature =
-    let s = Utils.hexToBigInt block |> Utils.decryptRSA e n
+let signature : byte [] =
+    let s = Utils.hexToBigInt block |> Utils.decryptRSA d n
     s.ToByteArray()
 
-//    |> Array.rev |> Array.append [|0uy|] |> Utils.bytesToHex
+let verify (s: byte []) (digestLen: int) : bool =
+    let s' = Utils.encryptRSA e n (BigInteger s)
+    let sHex = s'.ToByteArray() |> Array.rev |> Array.append [|0uy|]
+               |> Utils.bytesToHex
+    let i = (String.length sHex) - digestLen - 4
+    if sHex.[0..3] = "0001" && sHex.[(i..(i+3)] = "ff00" then true else false
+let testVerify = verify signature 40
