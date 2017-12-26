abe641dc453e27492afa1c9d8ad08e3ec0c38f12
tkuriyama <taro.kuriyama@gmail.com>
2017-12-25 23:51:36 -0500

    refactoring regex parsing, updating large prime

--- set6/p42.fsx
+++ set6/p42.fsx
@@ -5,6 +5,7 @@ open Utils
 open Sha1
 open System
 open System.Numerics
+open System.Text.RegularExpressions
 
 let ((e, n), (d, _)) = Utils.genRSAKeysSample
 
@@ -17,10 +18,22 @@ let signature : byte [] =
     let s = Utils.hexToBigInt block |> Utils.decryptRSA d n
     s.ToByteArray()
 
+let (|ParseRegex|_|) regex str =
+   let m = Regex(regex).Match(str)
+   if m.Success
+   then Some (List.tail [ for x in m.Groups -> x.Value ])
+   else None
+
+let parseSignature s : bool =
+    match s with
+    | ParseRegex "0001fff*00.*" s -> true
+    | _ -> false
+
 let verify (s: byte []) (digestLen: int) : bool =
     let s' = Utils.encryptRSA e n (BigInteger s)
-    let sHex = s'.ToByteArray() |> Array.rev |> Array.append [|0uy|]
-               |> Utils.bytesToHex
-    let i = (String.length sHex) - digestLen - 4
-    if sHex.[0..3] = "0001" && sHex.[(i..(i+3)] = "ff00" then true else false
+    s'.ToByteArray() |> Array.rev |> Array.append [|0uy|]
+    |> Utils.bytesToHex
+    |> parseSignature
+
 let testVerify = verify signature 40
+
--- set6/utils.fs
+++ set6/utils.fs
@@ -38,12 +38,6 @@ let repeat x = seq { while true do yield x }
 let repeatArr x n = repeat x |> Seq.take n |> Seq.toArray
 let repeatSeq xs = seq { while true do yield! xs }
 
-let (|ParseRegex|_|) regex str =
-   let m = Regex(regex).Match(str)
-   if m.Success
-   then Some (List.tail [ for x in m.Groups -> x.Value ])
-   else None
-
 (* Encodings *)
 
 let hexToByte = function
@@ -132,8 +126,9 @@ let rec genRSAKeys (r: Random) : ((BigInteger * BigInteger) * (BigInteger * BigI
     | _      -> genRSAKeys r
 
 let genRSAKeysSample =
-    let e = "ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff" |> hexToBigInt
-    let q = BigInteger 961957933
+    let p = "ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff" |> hexToBigInt
+    let e = BigInteger 3
+    let q = BigInteger 961957691
     let n = p * q
     let et = (p - (BigInteger 1)) * (q - (BigInteger 1))
     let d = modInvBig e et
