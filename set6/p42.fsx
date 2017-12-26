4332c0134e08dfb1dd9c347e1a225bcc05037bc0
tkuriyama <taro.kuriyama@gmail.com>
2017-12-25 20:26:48 -0500

    using very large fixed prime for exercise

--- set6/p42.fsx
+++ set6/p42.fsx
@@ -6,9 +6,15 @@ open Sha1
 open System
 open System.Numerics
 
+let ((e, n), (d, _)) = Utils.genRSAKeysSample
+
 let text = "hi mom" |> Utils.strToBytes 
 let digest = Sha1.sha1 text
-
-let signature =
+let block =
     let r = 128 - 3 - ((String.length digest) / 2)
     String.concat "" ["0001"; String.replicate r "ff"; "00"; digest]
+let signature =
+    let s = Utils.hexToBigInt block |> Utils.decryptRSA e n
+    s.ToByteArray()
+
+//    |> Array.rev |> Array.append [|0uy|] |> Utils.bytesToHex
--- set6/utils.fs
+++ set6/utils.fs
@@ -124,3 +124,20 @@ let rec genRSAKeys (r: Random) : ((BigInteger * BigInteger) * (BigInteger * BigI
     match d with
     | Some v -> ((e, n), (v, n))
     | _      -> genRSAKeys r
+
+let genRSAKeysSample =
+    let e = "ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff" |> hexToBigInt
+    let q = BigInteger 961957933
+    let n = p * q
+    let et = (p - (BigInteger 1)) * (q - (BigInteger 1))
+    let d = modInvBig e et
+    ((e, n), (d, n))
+
+let encryptRSA (e: BigInteger) (n: BigInteger) (m: BigInteger) =
+    if m > n then failwith "message out of range"
+    else BigInteger.ModPow (m, e, n)
+
+let decryptRSA (e: BigInteger) (n: BigInteger) (c: BigInteger) =
+    if c > n then failwith "message out of range"
+    else BigInteger.ModPow (c, e, n)
+
