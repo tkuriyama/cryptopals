e85484213bd42d375dc2af4de6dedde757fd9c78
tkuriyama <taro.kuriyama@gmail.com>
2018-01-13 16:59:52 -0800

    fixicng indentation

--- set6/utils.fs
+++ set6/utils.fs
@@ -166,21 +166,12 @@ let rec genRSAKeys (r: Random) (bits: int) : ((BigInteger * BigInteger) * (BigIn
     let p', q' = p - one, q - one
     match p = q || p' % e = zero || q' % e = zero with
     | true -> genRSAKeys r bits
-    | _     -> let n = p * q
-               let et = p' * q'
-               let d = modInvBig e et
-               match d with
-               | Some v -> ((e, n), (v, n))
-               | _      -> genRSAKeys r bits
-
-let genRSAKeysSample =
-    let p = "ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff" |> hexToBigInt
-    let e = BigInteger 3
-    let q = BigInteger 961957691
-    let n = p * q
-    let et = (p - (BigInteger 1)) * (q - (BigInteger 1))
-    let d = modInvBig e et
-    ((e, n), (d.Value, n))
+    | _    -> let n = p * q
+              let et = p' * q'
+              let d = modInvBig e et
+              match d with
+              | Some v -> ((e, n), (v, n))
+              | _      -> genRSAKeys r bits
 
 let encryptRSA (e: BigInteger) (n: BigInteger) (m: BigInteger) =
     if m > n then failwith "message out of range"
