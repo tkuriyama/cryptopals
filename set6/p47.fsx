fc532ff6d81caf9ae823bab2139f16ff539528a7
tkuriyama <taro.kuriyama@gmail.com>
2018-08-14 19:55:37 -0400

    adding tests

--- set6/p47.fsx
+++ set6/p47.fsx
@@ -7,6 +7,8 @@ open System.Numerics
 let rnd = new Random()
 let ((e, n), (d, _)) = Utils.genRSAKeys rnd 128
 
+(* Padding and Oracle *)
+
 let parityOracle d n c : bool =
     let m' = Utils.decryptRSA d n c
     let mArr = m'.ToByteArray() |> Array.rev
@@ -19,10 +21,16 @@ let genNonZeroRandArr (size: int) : byte [] =
     let rnd = Random()
     [|for _ in 1..size do yield rnd.Next 255 |> (+) 1 |> byte|]
 
-let padPKCS15 (m: string) =
+let padPKCS15 (m: string) : byte [] =
     let mArr = Utils.strToBytes m
     let k = n.ToByteArray() |> Array.length
     let PS = genNonZeroRandArr (k - 3 - (Array.length mArr))
-    Array.concat [| [| 0uy; 2uy |]; PS; [| 0uy |]; mArr |]
-    
-let c = padPKCS15 "kick it, CC" |> Array.rev |> BigInteger |> Utils.encryptRSA e n
+    Array.concat [| [| 0uy; 2uy |]; PS; [| 0uy |]; mArr |] |> Array.rev
+
+(* Test *)
+
+let padded = padPKCS15 "kick it, CC"
+let c = padded |> BigInteger |> Utils.encryptRSA e n
+let m  = Utils.decryptRSA d n c
+let mStr = m.ToByteArray() |> Array.rev |> Utils.bytesToStr
+let testParityOracle = parityOracle d n c
